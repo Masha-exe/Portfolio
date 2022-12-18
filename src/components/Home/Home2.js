35e3591fb94c17933152aa559998a4a6b3ddf2a6
Masha-exe <110632135+Masha-exe@users.noreply.github.com>
2022-12-19 00:52:37 +0300

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -16,39 +16,37 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              Voici une <span className="purple"> présentation </span> de la communauté
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              Kaori est une communauté amicale souhaitant accueillir une grande mass de personne dans un projet sociale, nous sommes une communauté grandement amicale et LGBTQ+
               <br />
-              <br />I am fluent in classics like
+              <br />Nous recrutons activement
               <i>
-                <b className="purple"> C++, Javascript and Python. </b>
+                <b className="purple"> C++, des CM et Staff. </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              Nous souhaitons entretenir une communauté saine pour tous merci &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
+                <b className="purple">de respecter nos conditions </b> and
+                vôtre aide compte{" "}
                 <b className="purple">
-                  Deep Learning and Natural Launguage Processing.
+                  beaucoup.
                 </b>
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              Si vous souhaitez faire partie de nos partenaire officiel merci de contacter Masha#1000 <b className="purple">Pour une échange de pub</b> contacter
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  un Community Manager
                 </b>
               </i>
-              &nbsp; like
+              &nbsp; nos CM
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> ont le droits de menner une enquête sur vôtre serveur pour la sécuritée de nos membres.</b>
               </i>
             </p>
           </Col>
@@ -60,14 +58,14 @@ function Home2() {
         </Row>
         <Row>
           <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
+            <h1>Retrouve nous sur</h1>
             <p>
               Feel free to <span className="purple">connect </span>with me
             </p>
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://discord.gg/kJhGEA6A5P"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -77,7 +75,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href="https://twitter.com/TRKaori"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -87,7 +85,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://kaoricafe.fr"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,7 +95,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/kaori.soc"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
