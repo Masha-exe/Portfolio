4217cb48dce9f26dec0ac76dfe065f51c85e1fb8
Masha-exe <110632135+Masha-exe@users.noreply.github.com>
2022-12-19 00:54:44 +0300

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Communauté amicale",
+          "Communauté LBTQ+",
+          "Actif et qui s'agrandis de jour en jour",
+          "Recrute activement",
         ],
         autoStart: true,
         loop: true,
